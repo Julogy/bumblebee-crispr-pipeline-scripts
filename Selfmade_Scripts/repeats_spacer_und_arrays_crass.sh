5cf1c1c2f47443229f45d609e97278e4660d115c
Michael Spies <jspies@hs-mittweida.de>
2026-05-03 11:54:05 +0200

    crass biological order

--- Selfmade_Scripts/repeats_spacer_und_arrays_crass.sh
+++ Selfmade_Scripts/repeats_spacer_und_arrays_crass.sh
@@ -1,93 +1,127 @@
 set -euo pipefail
 BASE="$1"
-# repeats und spacer rausholen und arrays mit zu vielen kurzen spacern raushauen
+# repeats und spacer rausholen 
 awk '
-function print_group(){
-    if (gid == "") return
+function out(){
+    if(!gid) return
+    print drh
+    print dr
 
-    print dr_header
-    print dr_seq
-
-    for(i=1;i<=spacer_count;i++){
-        print sp_header[i]
-        print sp_seq[i]
+    use_order=0
+    if(length(fs_next) > 0){
+        start=""
+        for(sid in cspacers){
+            if(!(sid in bs_prev)){
+                start=sid
+                break
+            }
+        }
+        delete visited
+        cur=start
+        cnt=0
+        while(cur != "" && !(cur in visited)){
+            visited[cur]=1
+            cnt++
+            cur=fs_next[cur]
+        }
+        if(cur=="") use_order=1
     }
-}
-BEGIN{
-    gid=""
+
+    if(use_order){
+        delete visited
+        cur=start
+        while(cur != "" && !(cur in visited)){
+            visited[cur]=1
+            if(cur in sp_seq){
+                print sp_head[cur]
+                print sp_seq[cur]
+            }
+            cur=fs_next[cur]
+        }
+        # Fehlende Spacer (nicht im Assembly) anhängen
+        for(i=1; i<=n; i++){
+            sid_xml=sph[i]
+            # Spacer-ID aus Header extrahieren
+            split(sid_xml, tmp, "|")
+            sid="SP"tmp[4]
+            if(!(sid in visited)){
+                print sph[i]
+                print sp[i]
+            }
+        }
+    } else {
+        for(i=1; i<=n; i++){
+            print sph[i]
+            print sp[i]
+        }
+    }
+
+    delete sp_seq; delete sp_head
+    delete cspacers; delete bs_prev; delete fs_next; delete visited
+    n=0; in_assembly=0; cur_cspacer=""
 }
 
 /<group .*drseq=/{
-    print_group()
-
-    match($0,/drseq="([ACGT]+)"/,dr)
+    out()
     match($0,/gid="G([0-9]+)"/,g)
-
-    gid=g[1]
-    dr_seq=dr[1]
-    dr_header=">DR|Group|" gid "|"
-
-    spacer_count=0
-    short_spacers=0
-    delete sp_header
-    delete sp_seq
+    match($0,/drseq="([ACGT]+)"/,d)
+    gid=g[1]; dr=d[1]
+    drh=">DR|Group|" gid "|"
 }
 
 /<spacer /{
-    match($0,/seq="([ACGT]+)"/,sp)
-    match($0,/spid="SP([0-9]+)"/,sid)
-
-    spacer_count++
-    sp_seq[spacer_count]=sp[1]
-    sp_header[spacer_count]=">SP|Group|" gid "|" sid[1] "|"
-
+    if(!in_assembly){
+        match($0,/spid="SP([0-9]+)"/,s)
+        match($0,/seq="([ACGT]+)"/,q)
+        sid="SP"s[1]
+        sp_head[sid]=">SP|Group|" gid "|" s[1] "|"
+        sp_seq[sid]=q[1]
+        sph[++n]=sp_head[sid]
+        sp[n]=q[1]
+    }
 }
 
-END{
-    print_group()
+/<assembly>/{ in_assembly=1 }
+/<\/assembly>/{ in_assembly=0 }
+
+in_assembly && /^[[:space:]]*<cspacer spid=/{
+    match($0,/spid="SP([0-9]+)"/,s)
+    cur_cspacer="SP"s[1]
+    cspacers[cur_cspacer]=1
 }
+
+in_assembly && /^[[:space:]]*<bs /{
+    match($0,/spid="SP([0-9]+)"/,s)
+    bs_prev[cur_cspacer]="SP"s[1]
+}
+
+in_assembly && /^[[:space:]]*<fs /{
+    match($0,/spid="SP([0-9]+)"/,s)
+    fs_next[cur_cspacer]="SP"s[1]
+}
+
+END{ out() }
 ' "$BASE/Crass/crass.crispr" > "$BASE/repeats_und_spacer/crass_R1_und_R2.fasta"
 
 #repeats und spacer in arrays umbauen bei crass
 awk '
-/^>/ {
-    header = substr($0,2)
-    split(header, a, "|")
-    type = a[1]
-    group = a[3]
-
-    if (type == "DR") {
-        dr[group] = ""
-        current_group = group
-    } else if (type == "SP") {
-        sp_index[group]++
-        current_group = group
-        current_sp = sp_index[group]
-    }
+/^>/{
+    split(substr($0,2),a,"|")
+    t=a[1]; g=a[3]
+    if(t=="SP") i[g]++
     next
 }
 
-{
-    if (type == "DR") {
-        dr[current_group] = $0
-    } else if (type == "SP") {
-        spacer[current_group, current_sp] = $0
-    }
-}
+t=="DR"{ dr[g]=$0 }
+t=="SP"{ sp[g,i[g]]=$0 }
 
-END {
-    for (g in dr) {
-        printf(">array|Group|%s|\n", g)
-
-        # Array korrekt zusammensetzen
-        printf("%s", dr[g])
-        for (i = 1; i <= sp_index[g]; i++) {
-            printf("%s%s", spacer[g,i], dr[g])
-        }
-        printf("\n")   # <<< WICHTIGER Zeilenumbruch
+END{
+    for(g in dr){
+        printf(">array|Group|%s|\n%s",g,dr[g])
+        for(j=1;j<=i[g];j++) printf("%s%s",sp[g,j],dr[g])
+        print ""
     }
-}
-' "$BASE/repeats_und_spacer/crass_R1_und_R2.fasta" > "$BASE/arrays/crass_R1_und_R2.fasta"
+}' "$BASE/repeats_und_spacer/crass_R1_und_R2.fasta" > "$BASE/arrays/crass_R1_und_R2.fasta"
 
 # im Ordner /home/jupyter-jspies/Ergebnisse/.../Crass/ zum aufräumen alles außer der .crispr löschen (weil nur die benutze ich)
 cd "$BASE/Crass/"
